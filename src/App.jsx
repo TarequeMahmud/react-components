1cb3004ea6acbff438e891939328e9ab5a1d5778
Tareque Mahmud <tarequemahmud313@gmail.com>
2024-08-12 16:58:20 +0600

    Image slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 // import Accordian from "./components/accordian/Accordian";
-import SearchAutocomplete from "./components/search-autocomplete/SearchAutocomplete";
+// import SearchAutocomplete from "./components/search-autocomplete/SearchAutocomplete";
+import ImageSlider from "./components/image-slider/ImageSlider";
 
 function App() {
   return (
     <div>
       {/* <Accordian /> */}
-      <SearchAutocomplete />
+      {/* <SearchAutocomplete /> */}
+      <ImageSlider />
     </div>
   );
 }
