3a10beb72d232406d7d0d5b988612710083586f3
Tareque Mahmud <tarequemahmud313@gmail.com>
2024-08-18 12:57:47 +0600

    Bug fixing

--- src/components/star-rating/StarRating.jsx
+++ src/components/star-rating/StarRating.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
+import "./styles.css";
 
 export default function StarRating({ noOfStar = 5 }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
-  const handleClick = (index) => setRating(index);
-  const hadleMouseEnter = (index) => setHover(index);
-  const handleMouseLeave = () => setHover(rating);
+  function handleClick(currentIndex) {
+    setRating(currentIndex);
+  }
+  function hadleMouseEnter(currentIndex) {
+    setHover(currentIndex);
+  }
+  function handleMouseLeave() {
+    setHover(rating);
+  }
 
   return (
     <div>
@@ -16,11 +23,7 @@ export default function StarRating({ noOfStar = 5 }) {
           <FaStar
             key={index}
             size={40}
-            style={
-              index <= (rating || hover)
-                ? { color: "#fff700" }
-                : { color: " #000000" }
-            }
+            className={index <= (hover || rating) ? "active" : "inactive"}
             onClick={() => handleClick(index)}
             onMouseMove={() => hadleMouseEnter(index)}
             onMouseLeave={() => handleMouseLeave()}
